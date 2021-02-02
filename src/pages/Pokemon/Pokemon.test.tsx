b68d770962e79586b16cafe66df0a5fe12b199c1
lbavendanos <lbavendanos@gmail.com>
2021-02-01 21:04:59 -0500

    Add content to pokemon page

--- src/pages/Pokemon/Pokemon.test.tsx
+++ src/pages/Pokemon/Pokemon.test.tsx
@@ -1,8 +1,16 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import Pokemon from './Pokemon'
 
 describe('Pokemon page', () => {
   it('render', () => {
     render(<Pokemon />)
+
+    const title = screen.getByText('POKEMON')
+    const subtitle = screen.getByText(
+      'In this section you can search for a pokemon'
+    )
+
+    expect(title).toBeInTheDocument()
+    expect(subtitle).toBeInTheDocument()
   })
 })
--- src/pages/Pokemon/Pokemon.tsx
+++ src/pages/Pokemon/Pokemon.tsx
@@ -1,5 +1,19 @@
+import PokemonList from '@/components/Sections/PokemonList/PokemonList'
+
 function Pokemon() {
-  return <div>Pokemon</div>
+  return (
+    <div className="container pt-16 mx-auto">
+      <div className="flex flex-col text-center w-full mb-5">
+        <h1 className="title sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
+          POKEMON
+        </h1>
+        <p className="subtitle lg:w-2/3 mx-auto leading-relaxed text-base">
+          In this section you can search for a pokemon
+        </p>
+      </div>
+      <PokemonList />
+    </div>
+  )
 }
 
 export default Pokemon
